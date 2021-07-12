12ae6bff7904acc9ca6a29725861015c2ddc4488
JulienScourneau <56632291+JulienScourneau@users.noreply.github.com>
2021-07-12 15:31:43 +0200

    Update AddEditEstateActivity layout

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/controller/AddEditEstateActivity.kt
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/controller/AddEditEstateActivity.kt
@@ -1,7 +1,10 @@
 package com.openclassrooms.realestatemanager.controller
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+import com.openclassrooms.realestatemanager.R
 import com.openclassrooms.realestatemanager.databinding.ActivityEstateBinding
 
 class AddEditEstateActivity : AppCompatActivity() {
@@ -12,5 +15,13 @@ class AddEditEstateActivity : AppCompatActivity() {
 
         binding = ActivityEstateBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val spinner: Spinner = binding.categorySpinner
+        ArrayAdapter.createFromResource(
+            this, R.array.category_spinner, android.R.layout.simple_spinner_item)
+            .also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                spinner.adapter = adapter
+            }
     }
 }
