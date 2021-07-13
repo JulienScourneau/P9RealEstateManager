a56ea374b182ccd6a16fc1ff229fd76744b3e9fa
JulienScourneau <56632291+JulienScourneau@users.noreply.github.com>
2021-07-13 15:33:31 +0200

    Adding EstateWithPhoto data class for room

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/controller/AddEditEstateActivity.kt
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/controller/AddEditEstateActivity.kt
@@ -5,23 +5,23 @@ import android.widget.ArrayAdapter
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import com.openclassrooms.realestatemanager.R
-import com.openclassrooms.realestatemanager.databinding.ActivityEstateBinding
+import com.openclassrooms.realestatemanager.databinding.AddEditActivityEstateBinding
 
 class AddEditEstateActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityEstateBinding
+    private lateinit var binding: AddEditActivityEstateBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityEstateBinding.inflate(layoutInflater)
+        binding = AddEditActivityEstateBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val spinner: Spinner = binding.categorySpinner
         ArrayAdapter.createFromResource(
-            this, R.array.category_spinner, android.R.layout.simple_spinner_item)
-            .also { adapter ->
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-                spinner.adapter = adapter
-            }
+            this, R.array.category_spinner, android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
     }
 }
--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/data/Estate.kt
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/data/Estate.kt
@@ -1,17 +1,24 @@
 package com.openclassrooms.realestatemanager.data
 
-
-import androidx.room.ColumnInfo
 import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.Relation
+
+data class EstateWithPhoto(
+    @Embedded val estate: Estate,
+    @Relation(
+
+        parentColumn = "id",
+        entityColumn = "estateId"
+    )
+    val photos: List<Photo> = emptyList()
+)
 
 
 @Entity(tableName = "estate_table")
-
 data class Estate(
 
-    //@Embedded var photos: Photo,
     var category: String,
     var price: String,
     var description: String,
@@ -23,8 +30,10 @@ data class Estate(
     @PrimaryKey(autoGenerate = true) val id: Int = 0
 )
 
+@Entity(tableName = "estate_photo")
 data class Photo(
-    var photo: List<Int>
+    @PrimaryKey var photo: String,
+    var estateId: Int
 )
 
 data class Address(
@@ -32,5 +41,5 @@ data class Address(
     var street: String,
     var city: String,
     var country: String,
-    @ColumnInfo var postalCode: Int
+    var postalCode: Int
 )
