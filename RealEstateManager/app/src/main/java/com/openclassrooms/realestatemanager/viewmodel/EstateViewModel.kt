17a9f08044b01e5a3c8381c75bb553e77246c94d
JulienScourneau <56632291+JulienScourneau@users.noreply.github.com>
2021-07-23 14:16:26 +0200

    Updated EstateViewModel with a function to work with the navigation component

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/viewmodel/EstateViewModel.kt
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/viewmodel/EstateViewModel.kt
@@ -4,9 +4,11 @@ import androidx.lifecycle.*
 import com.openclassrooms.realestatemanager.data.Estate
 import com.openclassrooms.realestatemanager.data.EstateWithPhoto
 import com.openclassrooms.realestatemanager.repository.EstateRepository
+import com.openclassrooms.realestatemanager.utils.ADD_ESTATE_RESULT_OK
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
-import java.lang.IllegalArgumentException
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,6 +16,8 @@ class EstateViewModel @Inject constructor(
     private val repository: EstateRepository
 ) : ViewModel() {
 
+    private val estateEventChannel = Channel<EstateEvent>()
+    val estateEvent = estateEventChannel.receiveAsFlow()
     val allEstate: LiveData<List<EstateWithPhoto>> = repository.allEstate.asLiveData()
 
     fun insert(estate: Estate) = viewModelScope.launch {
@@ -26,6 +30,30 @@ class EstateViewModel @Inject constructor(
 
     fun delete(estate: Estate) = viewModelScope.launch {
         repository.delete(estate)
+    }
+
+    fun onEstateSelected(estate: EstateWithPhoto) = viewModelScope.launch {
+        estateEventChannel.send(EstateEvent.NavigateToDetailsScreen(estate))
+    }
+
+    fun onAddNewEstateClick() = viewModelScope.launch {
+        estateEventChannel.send(EstateEvent.NavigateToAddEstateScreen)
+    }
+
+    fun onAddResult(result: Int, text: String) {
+        when (result) {
+            ADD_ESTATE_RESULT_OK -> showEstateAddedConfirmationMessage(text)
+        }
+    }
+
+    private fun showEstateAddedConfirmationMessage(text: String) = viewModelScope.launch {
+        estateEventChannel.send(EstateEvent.ShowEstateAddedConfirmationMessage(text))
+    }
+
+    sealed class EstateEvent {
+        object NavigateToAddEstateScreen : EstateEvent()
+        data class NavigateToDetailsScreen(val estate: EstateWithPhoto) : EstateEvent()
+        data class ShowEstateAddedConfirmationMessage(val msg: String) : EstateEvent()
     }
 
 }
