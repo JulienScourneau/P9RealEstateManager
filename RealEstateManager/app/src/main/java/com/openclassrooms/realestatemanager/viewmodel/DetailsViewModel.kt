1dc522b9ffc68dded4f4edd3554a82415fa39dca
JulienScourneau <56632291+JulienScourneau@users.noreply.github.com>
2021-07-26 11:38:20 +0200

    Updating DetailsViewModel to navigate in AddEditEstate fragment

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/viewmodel/DetailsViewModel.kt
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/viewmodel/DetailsViewModel.kt
@@ -1,12 +1,12 @@
 package com.openclassrooms.realestatemanager.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
+import androidx.lifecycle.*
 import com.openclassrooms.realestatemanager.data.EstateWithPhoto
 import com.openclassrooms.realestatemanager.repository.EstateRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,10 +16,18 @@ class DetailsViewModel @Inject constructor(
 ) : ViewModel() {
 
     val estateId = state.get<Long>("id")
+    private val detailsEstateEventChannel = Channel<DetailsEstateEvent>()
+    val estateEvent = detailsEstateEventChannel.receiveAsFlow()
 
     fun getEstateById(id: Long): LiveData<EstateWithPhoto?> {
         return repository.getEstateById(id).asLiveData()
     }
 
+    fun onEditEstateSelected(estate: EstateWithPhoto) = viewModelScope.launch {
+        detailsEstateEventChannel.send(DetailsEstateEvent.NavigateToEditEstateScreen(estate))
+    }
 
+    sealed class DetailsEstateEvent{
+        data class NavigateToEditEstateScreen(val estate: EstateWithPhoto) : DetailsEstateEvent()
+    }
 }
