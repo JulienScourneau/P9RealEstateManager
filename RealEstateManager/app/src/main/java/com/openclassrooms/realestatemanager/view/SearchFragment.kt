a0f1fe91388b7e620e26090ab560693498c6fc52
Scourneau Julien <meneurs@hotmail.com>
2021-08-10 14:37:08 +0200

    Create SearchFragment and  thefragment_search_estate layout

---
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/view/SearchFragment.kt
@@ -0,0 +1,7 @@
+package com.openclassrooms.realestatemanager.view
+
+import androidx.fragment.app.Fragment
+import com.openclassrooms.realestatemanager.R
+
+class SearchFragment : Fragment(R.layout.fragment_search_estate) {
+}
