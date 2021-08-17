b21308f05bf5f65d9d42c92b161da5b10d46b697
Scourneau Julien <meneurs@hotmail.com>
2021-08-17 15:24:31 +0200

    Update search estate layout and fragment to test expand/collapse view

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/view/SearchFragment.kt
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/view/SearchFragment.kt
@@ -2,6 +2,11 @@ package com.openclassrooms.realestatemanager.view
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import androidx.cardview.widget.CardView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.transition.AutoTransition
 import androidx.transition.TransitionManager
@@ -13,22 +18,26 @@ class SearchFragment : Fragment(R.layout.fragment_search_estate) {
     private lateinit var binding: FragmentSearchEstateBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding = FragmentSearchEstateBinding.bind(view)
-        setupListener()
 
+        binding.apply {
+            setupExpandView(arrowButtonPrice, priceLayout, baseLayoutPrice)
+        }
     }
 
-    private fun setupListener() {
-        binding.apply {
-            arrowButton.setOnClickListener {
-                if (hiddenView.visibility == View.VISIBLE) {
-                    TransitionManager.beginDelayedTransition(baseCardviewSurface, AutoTransition())
-                    hiddenView.visibility = View.GONE
-                    arrowButton.setImageResource(R.drawable.ic_baseline_expand_more_24)
-                } else {
-                    TransitionManager.beginDelayedTransition(baseCardviewSurface, AutoTransition())
-                    hiddenView.visibility = View.VISIBLE
-                    arrowButton.setImageResource(R.drawable.ic_baseline_expand_less_24)
-                }
+    private fun setupExpandView(
+        btn: ImageView,
+        expandLayout: LinearLayout,
+        baseLayout: LinearLayout
+    ) {
+        btn.setOnClickListener {
+            if (expandLayout.visibility == View.VISIBLE) {
+                expandLayout.visibility = View.GONE
+                TransitionManager.beginDelayedTransition(baseLayout, AutoTransition())
+                btn.setImageResource(R.drawable.ic_baseline_expand_more_24)
+            } else {
+                TransitionManager.beginDelayedTransition(baseLayout, AutoTransition())
+                expandLayout.visibility = View.VISIBLE
+                btn.setImageResource(R.drawable.ic_baseline_expand_less_24)
             }
         }
     }
