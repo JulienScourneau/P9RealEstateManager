a434c5537b45de4abf8cc34ce2d24565c6ba4809
JulienScourneau <56632291+JulienScourneau@users.noreply.github.com>
2021-08-22 20:28:41 +0200

    Updating Search model

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/data/Search.kt
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/data/Search.kt
@@ -1,20 +1,21 @@
 package com.openclassrooms.realestatemanager.data
 
 data class Search(
-    var category: String,
-    var minPrice: String,
-    var maxPrice: String,
-    var minArea: String,
-    var maxArea: String,
-    var room: String,
-    var bedroom: String,
-    var bathroom: String,
-    var photoAvailable: Boolean,
-    var city: String,
-    var postalCode: String,
-    var school: Boolean,
-    var localCommerce: Boolean ,
-    var publicTransport: Boolean ,
-    var park: Boolean,
-    var date: Long
+    var category: String?,
+    var minPrice: String?,
+    var maxPrice: String?,
+    var minArea: String?,
+    var maxArea: String?,
+    var minBedroom: String?,
+    var maxBedroom: String?,
+    var minBathroom: String?,
+    var maxBathroom: String?,
+    var photoAvailable: Boolean?,
+    var city: String?,
+    var postalCode: String?,
+    var school: Boolean?,
+    var localCommerce: Boolean?,
+    var publicTransport: Boolean?,
+    var park: Boolean?,
+    var date: Long?
     )
