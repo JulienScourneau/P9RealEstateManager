4b26240ce0e0081cd12b1f7833883131bc7fa374
JulienScourneau <56632291+JulienScourneau@users.noreply.github.com>
2021-08-23 15:06:54 +0200

    Update SearchFragment to setup radioGroups and category spinner

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/view/SearchFragment.kt
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/view/SearchFragment.kt
@@ -2,29 +2,49 @@ package com.openclassrooms.realestatemanager.view
 
 import android.os.Bundle
 import android.view.View
-import android.widget.ImageView
-import android.widget.LinearLayout
-import androidx.constraintlayout.widget.ConstraintLayout
+import android.widget.AdapterView
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.transition.AutoTransition
-import androidx.transition.TransitionManager
 import com.openclassrooms.realestatemanager.R
 import com.openclassrooms.realestatemanager.databinding.FragmentSearchEstateBinding
 import com.openclassrooms.realestatemanager.utils.Utils
 import com.openclassrooms.realestatemanager.viewmodel.SearchViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SearchFragment : Fragment(R.layout.fragment_search_estate) {
 
-    private lateinit var binding: FragmentSearchEstateBinding
     private val viewModel: SearchViewModel by viewModels()
+    private lateinit var binding: FragmentSearchEstateBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding = FragmentSearchEstateBinding.bind(view)
+        setupUI()
+    }
 
-        // setupEditText()
+    private fun setupUI() {
+        binding.apply {
+            textviewSchool.text = indifferentSchool.text
+            textviewLocalCommerce.text = indifferentLocalCommerce.text
+            textviewPark.text = indifferentPark.text
+            textviewPublicTransport.text = indifferentPublicTransport.text
+
+            searchCategorySpinner.onItemSelectedListener =
+                object : AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(
+                        adapter: AdapterView<*>?, view: View?,
+                        position: Int, id: Long
+                    ) {
+                        viewModel.searchCategory = adapter?.getItemAtPosition(position).toString()
+                    }
+
+                    override fun onNothingSelected(p0: AdapterView<*>?) {}
+                }
+        }
+        setupEditText()
         setupExpandView()
+        setupRadioButton()
     }
 
     private fun setupEditText() {
@@ -59,6 +79,66 @@ class SearchFragment : Fragment(R.layout.fragment_search_estate) {
                 }
                 editTextPostalCode.addTextChangedListener {
                     searchPostalCode = it.toString()
+                }
+            }
+        }
+    }
+
+    private fun setupRadioButton() {
+        binding.apply {
+            radioGroupSchool.setOnCheckedChangeListener { radioGroup, id ->
+                when (id) {
+                    R.id.indifferent_school -> {
+                        textviewSchool.text = indifferentSchool.text
+                        viewModel.searchSchool = null
+                    }
+                    R.id.with_school -> {
+                        textviewSchool.text = withSchool.text
+                        viewModel.searchSchool = true
+                    }
+                    R.id.without_school -> {
+                        textviewSchool.text = withoutSchool.text
+                        viewModel.searchSchool = false
+                    }
+                }
+            }
+            radioGroupLocalCommerce.setOnCheckedChangeListener { _, id ->
+                when (id) {
+                    R.id.indifferent_local_commerce -> {
+                        textviewLocalCommerce.text = indifferentLocalCommerce.text
+                    }
+                    R.id.with_local_commerce -> {
+                        textviewLocalCommerce.text = withLocalCommerce.text
+                    }
+                    R.id.without_local_commerce -> {
+                        textviewLocalCommerce.text = withoutLocalCommerce.text
+                    }
+                }
+            }
+            radioGroupPark.setOnCheckedChangeListener { _, id ->
+                when (id) {
+                    R.id.indifferent_park -> {
+                        textviewPark.text = indifferentPark.text
+                    }
+                    R.id.with_park -> {
+                        textviewPark.text = withPark.text
+                    }
+                    R.id.without_park -> {
+                        textviewPark.text = withoutPark.text
+                    }
+                }
+            }
+            radioGroupPublicTransport.setOnCheckedChangeListener { _, id ->
+                when (id) {
+                    R.id.indifferent_public_transport -> {
+                        textviewPublicTransport.text = indifferentPublicTransport.text
+                    }
+                    R.id.with_public_transport -> {
+                        textviewPublicTransport.text = withPublicTransport.text
+                    }
+                    R.id.without_public_transport -> {
+                        textviewPublicTransport.text = withoutPublicTransport.text
+                    }
                 }
             }
         }
