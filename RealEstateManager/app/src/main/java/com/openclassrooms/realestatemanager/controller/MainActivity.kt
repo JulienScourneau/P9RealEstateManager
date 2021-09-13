3efb57f608045234416daa80e11f310f78f4e5ca
JulienScourneau <56632291+JulienScourneau@users.noreply.github.com>
2021-09-13 16:57:59 +0200

    Add request permissions in MainActivity

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/controller/MainActivity.kt
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/controller/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.openclassrooms.realestatemanager.controller
 
+import android.Manifest
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.openclassrooms.realestatemanager.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private val requestMultiPermission =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach {
+                Log.d("permissions", "${it.key}: ${it.value}")
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        requestMultiPermission.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
     }
 }
