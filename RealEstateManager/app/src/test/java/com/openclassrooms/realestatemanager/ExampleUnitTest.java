e659513d04db7fc067dcf467b12208eaa9b7008c
JulienScourneau <56632291+JulienScourneau@users.noreply.github.com>
2021-09-20 16:49:41 +0200

    Add unit test for convert Dollar/Euro

--- RealEstateManager/app/src/test/java/com/openclassrooms/realestatemanager/ExampleUnitTest.java
+++ RealEstateManager/app/src/test/java/com/openclassrooms/realestatemanager/ExampleUnitTest.java
@@ -1,5 +1,7 @@
 package com.openclassrooms.realestatemanager;
 
+import com.openclassrooms.realestatemanager.utils.Utils;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -11,7 +13,16 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void convertDollarToEuroIsCorrect() throws Exception {
+        int dollar = 1000;
+        dollar = Utils.convertDollarToEuro(dollar);
+        assertEquals(812,dollar);
+    }
+
+    @Test
+    public void convertEuroToDollarIsCorrect() throws Exception {
+        int euro = 1000;
+        euro = Utils.convertEuroToDollar(euro);
+        assertEquals(1232,euro);
     }
 }
