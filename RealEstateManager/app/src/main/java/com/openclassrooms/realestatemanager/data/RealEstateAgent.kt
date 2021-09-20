18ed23095925e2232975778ca7f712b11384bb04
JulienScourneau <56632291+JulienScourneau@users.noreply.github.com>
2021-09-20 12:02:36 +0200

    Update RealEstateAgent.kt

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/data/RealEstateAgent.kt
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/data/RealEstateAgent.kt
@@ -5,4 +5,8 @@ import java.io.Serializable
 data class RealEstateAgent(
     var name: String,
     var phoneNumber: String
-) : Serializable
+) : Serializable {
+    override fun toString(): String {
+        return name
+    }
+}
