aea495a236a52fd9f6ca6fa378fff94955ee7832
JulienScourneau <56632291+JulienScourneau@users.noreply.github.com>
2021-09-27 14:31:53 +0200

    Add event to display edit message

--- RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/viewmodel/DetailsViewModel.kt
+++ RealEstateManager/app/src/main/java/com/openclassrooms/realestatemanager/viewmodel/DetailsViewModel.kt
@@ -1,16 +1,13 @@
 package com.openclassrooms.realestatemanager.viewmodel
 
 import androidx.lifecycle.*
-import com.openclassrooms.realestatemanager.data.Estate
 import com.openclassrooms.realestatemanager.data.EstateWithPhoto
-import com.openclassrooms.realestatemanager.data.Photo
 import com.openclassrooms.realestatemanager.repository.EstateRepository
+import com.openclassrooms.realestatemanager.utils.EDIT_ESTATE_RESULT_OK
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
-import java.util.ArrayList
 import javax.inject.Inject
 
 @HiltViewModel
@@ -22,17 +19,25 @@ class DetailsViewModel @Inject constructor(
     val estateId = state.get<Long>("id")
     private val detailsEstateEventChannel = Channel<DetailsEstateEvent>()
     val estateEvent = detailsEstateEventChannel.receiveAsFlow()
-    val estateWithPhoto: LiveData<EstateWithPhoto> = estateId?.let { repository.getEstateById(it).asLiveData() }!!
-
-    fun getEstateById(id: Long): LiveData<EstateWithPhoto?> {
-        return repository.getEstateById(id).asLiveData()
-    }
+    val estateWithPhoto: LiveData<EstateWithPhoto> =
+        estateId?.let { repository.getEstateById(it).asLiveData() }!!
 
     fun onEditEstateSelected(estate: EstateWithPhoto) = viewModelScope.launch {
         detailsEstateEventChannel.send(DetailsEstateEvent.NavigateToEditEstateScreen(estate))
     }
 
+    fun onEditResult(result: Int, text: String) {
+        when (result) {
+            EDIT_ESTATE_RESULT_OK -> showEstateUpdatedConfirmationMessage(text)
+        }
+    }
+
+    private fun showEstateUpdatedConfirmationMessage(text: String) = viewModelScope.launch {
+        detailsEstateEventChannel.send(DetailsEstateEvent.ShowEstateUpdatedConfirmationMessage(text))
+    }
+
     sealed class DetailsEstateEvent {
         data class NavigateToEditEstateScreen(val estate: EstateWithPhoto) : DetailsEstateEvent()
+        data class ShowEstateUpdatedConfirmationMessage(val msg: String) : DetailsEstateEvent()
     }
 }
